55b3f1c5369d7d8f723b0a8df665a9f4ae404d62
Irvan Castro <castroirvan@gmail.com>
2024-01-18 17:50:54 +0800

    Make gameboard and cell lowercased to keep consistency

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // The Gameboard represents the state of the game
-function Gameboard() {
+function gameboard() {
     const board = [];
 
     // Create a 2D array that will reperesent the game board
     for (let i = 0; i < 3; i++) {
         board[i] = []
         for (let j = 0; j < 3; j++) {
-            board[i].push(Cell());
+            board[i].push(cell());
         }
     }
 
@@ -35,14 +35,13 @@ function Gameboard() {
     };
 }
 
-
 /*
 A Cell represents a box in the game board and can have one of three values:
 0: Box is empty
 1: Player 1's token
 2: Player 2's token
 */
-function Cell() {
+function cell() {
     let value = 0;
   
     // Accept a player's token to change the value of the cell
