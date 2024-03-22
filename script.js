f8cb2c74ff3f31fb6526e6bc06d9785cd2980cba
Irvan Castro <castroirvan@gmail.com>
2024-03-22 13:34:28 +0800

    Refactor gameboard and gameController FF to class-based implementation
    - Commented the gameboard and gameController FF implementation
    - Improved cellClickHandler function to allow bubbling for <i> tags

--- script.js
+++ script.js
@@ -1,54 +1,100 @@
+// Game module 
 const game = (() => {
     // The gameboard represents the state of the game
-    function gameboard() {
-        const board = [];
+    // function gameboard() {
+    //     const board = [];
     
-        // Create a 2D array that will reperesent the game board
-        for (let i = 0; i < 3; i++) {
-            board[i] = []
-            for (let j = 0; j < 3; j++) {
-                board[i].push(new cell());
+    //     // Create a 2D array that will reperesent the game board
+    //     for (let i = 0; i < 3; i++) {
+    //         board[i] = []
+    //         for (let j = 0; j < 3; j++) {
+    //             board[i].push(new cell());
+    //         }
+    //     }
+    
+    //     // Getter function to retrieve the board through closure
+    //     const getBoard = () => board;
+    
+    //     // Add player's token to the board
+    //     const addMove = (x, y, player, gameState) => {
+    //         const selectedCell = board[x][y];
+    
+    //         // Check if cell is empty or game is ongoing
+    //         if (selectedCell.getValue() === 0 & gameState === 0) {
+    //             selectedCell.addToken(player);
+    //             return true;
+    //         }
+            
+    //         return false;
+    //     };
+    
+    //     // Print board to console
+    //     const printBoard = () => {
+    //         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    //         console.log(boardWithCellValues);
+    //     };
+
+    //     // Reset the board
+    //     const resetBoard = () => {
+    //         for (let i = 0; i < 3; i++) {
+    //             for (let j = 0; j < 3; j++) {
+    //                 board[i][j].clearCell();
+    //             }
+    //         }
+    //     }
+    
+    //     return {
+    //         addMove,
+    //         getBoard, 
+    //         printBoard,
+    //         resetBoard
+    //     };
+    // }
+
+    // ***GAMEBOARD CLASS IMPLEMENTATION***
+    class gameboard {
+        #board = [];
+
+        constructor() {
+             // Create a 2D array that will reperesent the game board
+            for (let i = 0; i < 3; i++) {
+                this.#board[i] = []
+                for (let j = 0; j < 3; j++) {
+                    this.#board[i].push(new cell());
+                }
             }
         }
-    
-        // Getter function to retrieve the board through closure
-        const getBoard = () => board;
-    
-        // Add player's token to the board
-        const addMove = (x, y, player, gameState) => {
-            const selectedCell = board[x][y];
-    
-            // Check if cell is empty or game is ongoing
+
+        getBoard() {
+            return this.#board;
+        }
+
+        addMove(x, y, player, gameState) {
+            const selectedCell = this.#board[x][y];
+
             if (selectedCell.getValue() === 0 & gameState === 0) {
                 selectedCell.addToken(player);
                 return true;
             }
-            
-            return false;
-        };
-    
-        // Print board to console
-        const printBoard = () => {
-            const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
-            console.log(boardWithCellValues);
-        };
 
-        // Reset the board
-        const resetBoard = () => {
+            return false;
+        }
+
+        resetBoard() {
             for (let i = 0; i < 3; i++) {
                 for (let j = 0; j < 3; j++) {
-                    board[i][j].clearCell();
+                    this.#board[i][j].clearCell();
                 }
             }
         }
-    
-        return {
-            addMove,
-            getBoard, 
-            printBoard,
-            resetBoard
-        };
+
+        printBoard() {
+            const boardWithCellValues = this.#board.map((row) => row.map((cell) => cell.getValue()))
+            console.log(boardWithCellValues);
+        }
+        
     }
+
 
     /*
     A cell represents a box in the game board and can have one of three values:
@@ -57,7 +103,6 @@ const game = (() => {
     2: Player 2's token
     */
 
-    // ***CELL FACTORY FUNCTION IMPLEMENTATION***
     // function cell() {
     //     let value = 0;
       
@@ -135,11 +180,103 @@ const game = (() => {
     }
     
     // The gameController controls flow of the game
-    function gameController() {
-        const board = gameboard();
-        let status = 0;
+    // function gameController() {
+    //     const board = new gameboard();
+    //     let status = 0;
     
-        const players = [
+    //     const players = [
+    //         {
+    //             name: "Player One",
+    //             token: 1,
+    //             score: 0
+    //         },
+    //         {
+    //             name: "Player Two",
+    //             token: 2,
+    //             score: 0
+    //         }
+    //     ];
+    
+    //     let activePlayer = players[0];
+    
+    //     // Get current active player
+    //     const getActivePlayer = () => activePlayer;
+    
+    //     // Switch player turn
+    //     const switchPlayerTurn = () => {
+    //         activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    //     };
+
+    //     // Add player score
+    //     const addPlayerScore = (player) => {
+    //         players[player].score++;
+    //     };
+
+    //     // Get current game status
+    //     const getGameStatus = () => status;
+
+    //     // Get players' scores
+    //     const getPlayersScores = () => [players[0].score, players[1].score];
+
+    //     // Change player name
+    //     const changePlayerName = (player, newName) => {
+    //         players[player].name = newName;
+    //     }
+    
+    //     const playRound = (x, y) => {
+    //         let gameState = checkGameState(board.getBoard()); 
+            
+    //         // Check if a move is valid
+    //         if (!board.addMove(x, y, getActivePlayer().token, gameState)) return;
+
+    //         // Check for a winner or tie after making move
+    //         gameState = checkGameState(board.getBoard());
+
+    //         const statusContainer = document.querySelector(".status");
+
+    //         // Check if status is 0 (game ongoing) or 1 (game ended)
+    //         if (status == 0) {
+    //             // Check for a tie or winner
+    //             if (gameState === 1 || gameState === 2) {
+    //                 addPlayerScore(gameState - 1);
+    //                 status = 1;
+    //                 statusContainer.textContent = `${players[gameState - 1].name} Won!`
+    //             }
+    //             else if (gameState === 3) {
+    //                 status = 1;
+    //                 statusContainer.textContent = "Tie!";
+    //             }
+
+    //             switchPlayerTurn();
+    //             board.printBoard();
+    //         }
+
+    //         return;
+    //     }
+
+    //     const reset = () => {
+    //         board.resetBoard();
+    //         status = 0;
+    //     }
+    
+    //     board.printBoard();
+        
+    //     return {
+    //         playRound,
+    //         getActivePlayer,
+    //         switchPlayerTurn,
+    //         getBoard: board.getBoard,
+    //         getGameStatus,
+    //         getPlayersScores,
+    //         changePlayerName,
+    //         reset
+    //     };
+    // }
+    
+    // ***GAMECONTROLLER CLASS IMPLEMENTATION***
+    class gameController {
+        #status = 0;
+        #players = [
             {
                 name: "Player One",
                 token: 1,
@@ -150,85 +287,79 @@ const game = (() => {
                 token: 2,
                 score: 0
             }
-        ];
-    
-        let activePlayer = players[0];
-    
-        // Get current active player
-        const getActivePlayer = () => activePlayer;
-    
-        // Switch player turn
-        const switchPlayerTurn = () => {
-            activePlayer = activePlayer === players[0] ? players[1] : players[0];
-        };
+        ]
+        #activePlayer;
 
-        // Add player score
-        const addPlayerScore = (player) => {
-            players[player].score++;
-        };
-
-        // Get current game status
-        const getGameStatus = () => status;
-
-        // Get players' scores
-        const getPlayersScores = () => [players[0].score, players[1].score];
-
-        // Change player name
-        const changePlayerName = (player, newName) => {
-            players[player].name = newName;
+        constructor() {
+            this.board = new gameboard()
+            this.#activePlayer = this.#players[0];
+            this.board.printBoard();
         }
-    
-        const playRound = (x, y) => {
-            let gameState = checkGameState(board.getBoard()); 
-            
+
+        getActivePlayer() {
+            return this.#activePlayer;
+        }
+
+        switchPlayerTurn() {
+            this.#activePlayer = this.#activePlayer === this.#players[0] ? this.#players[1] : this.#players[0];
+        }
+
+        addPlayerScore(player) {
+            this.#players[player].score++;
+        }
+
+        getGameStatus() {
+            return this.#status;
+        }
+
+        getPlayersScores() {
+            return [this.#players[0].score, this.#players[1].score];
+        }
+
+        changePlayerName(player, newName) {
+            this.#players[player].name = newName;
+        }
+
+        playRound(x, y) {
+            let gameState = checkGameState(this.board.getBoard());
+
             // Check if a move is valid
-            if (!board.addMove(x, y, getActivePlayer().token, gameState)) return;
+            if (!this.board.addMove(x, y, this.getActivePlayer().token, gameState)) return;
 
             // Check for a winner or tie after making move
-            gameState = checkGameState(board.getBoard());
+            gameState = checkGameState(this.board.getBoard());
 
             const statusContainer = document.querySelector(".status");
 
             // Check if status is 0 (game ongoing) or 1 (game ended)
-            if (status == 0) {
+            if (this.#status == 0) {
                 // Check for a tie or winner
                 if (gameState === 1 || gameState === 2) {
-                    addPlayerScore(gameState - 1);
-                    status = 1;
-                    statusContainer.textContent = `${players[gameState - 1].name} Won!`
+                    this.addPlayerScore(gameState - 1);
+                    this.#status = 1;
+                    statusContainer.textContent = `${this.#players[gameState - 1].name} Won!`
                 }
                 else if (gameState === 3) {
-                    status = 1;
+                    this.#status = 1;
                     statusContainer.textContent = "Tie!";
                 }
 
-                switchPlayerTurn();
-                board.printBoard();
+                this.switchPlayerTurn();
+                this.board.printBoard();
             }
-
-            return;
         }
 
-        const reset = () => {
-            board.resetBoard();
-            status = 0;
+        getBoard() {
+            return this.board.getBoard();
         }
-    
-        board.printBoard();
-        
-        return {
-            playRound,
-            getActivePlayer,
-            switchPlayerTurn,
-            getBoard: board.getBoard,
-            getGameStatus,
-            getPlayersScores,
-            changePlayerName,
-            reset
-        };
+
+        reset() {
+            this.board.resetBoard();
+            this.#status = 0;
+        }
     }
-    
-    return gameController();
+
+    return new gameController();
 })();
 
 function displayController() {
@@ -298,8 +429,11 @@ function displayController() {
 
     // Add click handler for the board's event listener
     function cellClickHandler(e) {
-        const x = e.target.dataset.row;
-        const y = e.target.dataset.col;
+        const targetElement = e.target.closest('.cell'); 
+        if (!targetElement) return;
+
+        const x = targetElement.dataset.row;
+        const y = targetElement.dataset.col;
 
         game.playRound(x, y);
         updateDisplay();
